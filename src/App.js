e1dfdbd32d4658d1e039d380a2feac185d7acd7b
Zima-Zema <blackflower225@gmail.com>
2018-02-12 17:12:11 +0200

    discover the component State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+
 class App extends Component {
+
+  state={
+    persons:[
+      {name:'Zima',age:25},
+      {name:'Tamer',age:30},
+      {name:'Mohamed',age:24}
+      
+    ]
+  }
+  switchNameHandler=()=>{
+    this.state.persons[0].name="Abdelazim";
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi This is React App</h1>
-      <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >I'm in love with Fatma</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      
       </div>
     );
 
